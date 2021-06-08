54fca9a4bbc8d242f5b3151e0516fe39fe277dc8
Roman Shtylman <roman@foxglove.dev>
2021-06-08 09:51:38 -0700

    Change PanelToolbar to display when in edit layout mode.

--- packages/studio-base/src/panels/Plot/index.tsx
+++ packages/studio-base/src/panels/Plot/index.tsx
@@ -258,7 +258,6 @@ function Plot(props: Props) {
 
   return (
     <Flex col clip center style={{ position: "relative" }}>
-      <PanelToolbar helpContent={helpContent} floating />
       <PlotChart
         paths={yAxisPaths}
         minYValue={parseFloat((minYValue ?? "")?.toString())}
